32c836b4d5bce01d310ddf85550ad45d40d07bf9
kennethgrau <kengrau@tamu.edu>
2022-04-21 02:02:26 +0100

    Adding L3 and cleaning L1

---
+++ software/L3_LEDcontrol.py
@@ -0,0 +1,4 @@
+# Control protocol for automatic LED control through LIDAR
+# Turns on LEDS when rebot believes the arm is over a surface that
+# can be sanitized by the arm.
+# Updated 4/20/2022 - Kenneth Grau
---
+++ software/L3_obstacleAvoid.py
@@ -0,0 +1,6 @@
+# This program includes the obstacle avoidance protocols for CAST robot
+# Includes the use of ToF sensors to check planes of arm for obstacles
+# similar to how a garage door detects objects.
+# This provides automatic control of the arm. Manual control is available in
+# L3_gpControl.py
+# Updated 4/20/2022 - Kenneth Grau
